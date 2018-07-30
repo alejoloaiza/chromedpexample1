014e84601696541a26dee1faf14e45f501abb63a
alejoloaiza <alejo.loaiza@gmail.com>
2018-07-30 08:37:36 -0500

    [Add] Added Multiple travels

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-var startdate, enddate, origin, destination, logpath string
+var startdate, enddate, origin, destination, mid, logpath string
 var aproxduration int
 
 func main() {
@@ -28,11 +28,11 @@ func main() {
 			destination = os.Args[5]
 			logpath = os.Args[6]
 		} else {
-			startdate = "190123"
-			enddate = "190228"
-			origin = "mdea"
+			startdate = "180801"
+			enddate = "181031"
+			origin = "sins"
 			destination = "syda"
-			logpath = "results.csv"
+			logpath = "resultssin-syd.csv"
 		}
 		InitCaptureOneWay(logpath, startdate, enddate, origin, destination)
 
@@ -45,14 +45,32 @@ func main() {
 			destination = os.Args[6]
 			logpath = os.Args[7]
 		} else {
-			startdate = "190123"
-			enddate = "190228"
-			aproxduration = 20
-			origin = "mdea"
-			destination = "syda"
-			logpath = "results.csv"
+			startdate = "181120"
+			enddate = "190110"
+			aproxduration = 45
+			origin = "syda"
+			destination = "mdea"
+			logpath = "resultsjuan.csv"
 		}
 		InitCaptureReturn(logpath, startdate, enddate, aproxduration, origin, destination)
+
+	} else if os.Args[1] == "multiple" {
+		if len(os.Args) > 6 {
+			startdate = os.Args[2]
+			enddate = os.Args[3]
+			origin = os.Args[4]
+			mid = os.Args[5]
+			destination = os.Args[6]
+			logpath = os.Args[7]
+		} else {
+			startdate = "2018-08-01"
+			enddate = "2018-10-31"
+			origin = "mdea"
+			mid = "scla"
+			destination = "syda"
+			logpath = "resultsmed-scl-syd.csv"
+		}
+		InitCaptureMultiple(logpath, startdate, enddate, origin, mid, destination)
 
 	}
 
